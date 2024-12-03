d88934095173848efa24898884d36faff732e41f
kunalWebDev07 <168664400+kunalkr07@users.noreply.github.com>
2024-12-04 03:40:10 +0530

    remove header in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
