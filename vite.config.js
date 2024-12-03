1e2d92ab768c181c74780b739d8ad6c9f89df938
kunalWebDev07 <168664400+kunalkr07@users.noreply.github.com>
2024-12-04 04:01:19 +0530

    adde in one array stuct..

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome'],
-      external: ['@fortawesome/free-solid-svg-icons'],
+      external: [
+        '@fortawesome/react-fontawesome',
+        '@fortawesome/free-solid-svg-icons',
+      ],
     },
   },
-})
+});
